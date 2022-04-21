0cf2c6b32d4d489b061d805ca3badf89e05f448c
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 17:44:21 -0300

    webmock

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,8 @@
 describe 'Httparty' do
-  it 'Httparty' do
+  it 'Content-Type' do
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+      to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     puts content_type
