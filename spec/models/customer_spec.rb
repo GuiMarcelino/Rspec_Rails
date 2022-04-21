a1eaf6cf0c97e32d20a474f603994d0d9787093a
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 13:51:53 -0300

    usando traits

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -33,5 +33,25 @@ RSpec.describe Customer, type: :model do
     expect(customer.name.upcase).to eq(customer.name)
   end
 
+  it 'Cliente Masculino' do
+    customer = create(:customer_male)
+    expect(customer.gender).to eq('M')
+  end
+
+  it 'Cliente Masculino Vip' do
+    customer = create(:customer_male_vip)
+    expect(customer.gender).to eq('M')
+    expect(customer.vip).to eq(true)
+  end
+
+  it 'Cliente Feminino' do
+    customer = create(:customer_female)
+    expect(customer.gender).to eq('F')
+  end
+
+  it 'Cliente Feminino Default' do
+    customer = create(:customer_female_default)
+    expect(customer.gender).to eq('F')
+  end
 
 end
