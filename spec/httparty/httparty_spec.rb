00ce5ed5399a420126f05276d5974505754cf058
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 18:13:01 -0300

    vcr com uri nao determinadas

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,9 +1,9 @@
 describe 'Httparty' do
-  it 'Content-Type', vcr: { cassette_name: 'jsonplaceholder/posts'} do
+  it 'Content-Type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
     #
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
 
