7dd447bebe2dce1573e9a456dff468d2e520feff
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 10:22:21 -0300

    usando fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :all
+
+  it 'Create a Customer' do
+    customer = customers(:guilherme)
+    expect(customer.full_name).to eq("Sr. Guilherme Marcelino")
+  end
 end
