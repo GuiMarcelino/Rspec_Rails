17a020f71aca2908fcf43b1804a54f118bf9d9f4
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 10:39:18 -0300

    sobreescrevendo atributos e utilizando aliases

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
 
   it 'full_name' do
-    customer = create(:customer)
+    customer = create(:user) # por conta do alias podemos usar customer ou user
     expect(customer.full_name).to start_with("Sr. ")
   end
+
+  it 'sobre-escrever atributos' do
+    customer = create(:customer, name: "Guilherme Marcelino" )
+    expect(customer.full_name).to eq("Sr. Guilherme Marcelino")
+  end
+
   # testando se vou criado um customer pela quatidade de customers existente depois da criação
   it { expect{ create(:customer) }.to change {Customer.all.size}.by(1) }
 
