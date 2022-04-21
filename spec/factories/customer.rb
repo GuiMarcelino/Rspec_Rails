aa93d51d076098c17f33d2d7574aae8a26a45608
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 10:30:14 -0300

    utilizando primeira vez factorybot

---
+++ spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name "Guilherme Marcelino"
+    email "guilherme@gmail.com"
+  end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  fixtures :all
 
   it 'Create a Customer' do
-    customer = customers(:guilherme)
+    customer = create(:customer)
     expect(customer.full_name).to eq("Sr. Guilherme Marcelino")
   end
 end
