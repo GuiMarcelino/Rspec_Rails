f7a1cf699e5eb53a63345594dca3f4ef47405c1d
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 18:18:05 -0300

    vcr modos de gravação

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,11 +1,12 @@
 describe 'Httparty' do
-  it 'Content-Type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
-    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+  # it 'Content-Type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
+  it 'Content-Type', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do
+
+  # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
-    #
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
+
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
-
   end
 end
