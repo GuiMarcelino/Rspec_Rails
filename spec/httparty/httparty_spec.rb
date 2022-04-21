4cc9e2623df976bb19fe782af7b283a018619d3d
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-21 17:56:43 -0300

    utilizando VCR

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,12 +1,11 @@
 describe 'Httparty' do
   it 'Content-Type' do
-    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
-      to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
-
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-    content_type = response.headers['content-type']
-    puts content_type
-    expect(content_type).to match(/application\/json/)
+    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+    #   to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
+    VCR.use_cassette("jsonplaceholder/posts") do
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+      content_type = response.headers['content-type']
+      expect(content_type).to match(/application\/json/)
+    end
   end
-
 end
