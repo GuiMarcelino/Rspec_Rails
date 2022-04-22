61db7a91d42dc78a9dd5a5facd77c76fba3e3bd4
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-22 16:36:34 -0300

    primeiro test com models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # teste do model com os todos os atributos validos
+  it 'is valid with description, price and category' do
+    product = create(:product)
+    expect(product).to be_valid
+  end
+
+  # 3 testes para campos nulos description, price e category
+  it 'is invalid without description' do
+    product = build(:product, description: nil)
+    product.valid?
+    expect(product.errors[:description]).to include("can't be blank")
+  end
+
+  it 'is invalid without price' do
+    product = build(:product, price: nil)
+    product.valid?
+    expect(product.errors[:price]).to include("can't be blank")
+  end
+
+  it 'is invalid without category' do
+    product = build(:product, category: nil)
+    product.valid?
+    expect(product.errors[:category]).to include("can't be blank")
+  end
+
+  # teste método de classe
+  it 'return a product with a full description' do
+    product = create(:product)
+    expect(product.full_description).to eq("#{product.description} - #{product.price}")
+  end
+
+
 end
