a56f6c42345424eb9fa40684241e4b20511f059b
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-23 15:43:56 -0300

    teste json index e show

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -26,5 +26,21 @@ RSpec.describe "Customers", type: :request do
                                  email: (be_kind_of String)
                                             )
     end
+
+    # it 'create - JSON' do
+    #   member = create(:member)
+    #   debugger
+    #   login_as(member, scope: :member)
+    #
+    #   headers = { "ACCEPT" => "application/json" }
+    #
+    #   customers_params = attributes_for(:customer)
+    #   post "/customers.json", params: { custom: customers_params}, headers: headers
+    #   expect(response.body).to include_json(
+    #                              id: /\d/,
+    #                              name: (be_kind_of String),
+    #                              email: (be_kind_of String)
+    #                            )
+    # end
   end
 end
