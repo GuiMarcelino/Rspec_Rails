f1249ffa215dc84f75d50cf811646c2f450507ac
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-23 08:43:03 -0300

    testando controllers com content type json

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -15,22 +15,47 @@ RSpec.describe CustomersController, type: :controller do
     end
   end
 
-  context 'Testando usuário não autenticado não pode fazer essa ação retorna status code 302  não autorizado' do
-    it 'returns a http status 302 (not authorizad)' do
-      customer = create(:customer)
-      get :show, params: { id: customer.id }
-      expect(response).to have_http_status(302)
+  describe 'as Logged Member' do
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
     end
-  end
 
-  context  "Testando usuario com autorização" do
-    it '#show' do
-      member = create(:member)
-      customer = create(:customer)
+    context 'Testando usuário não autenticado não pode fazer essa ação retorna status code 302  não autorizado' do
+      it 'returns a http status 302 (not authorizad)' do
+        get :show, params: { id: @customer.id }
+        expect(response).to have_http_status(302)
+      end
+    end
 
-      sign_in member
-      get :show, params: { id: customer.id }
-      expect(response).to have_http_status(200)
+    context "Testando usuario com autorização" do
+      it '#show' do
+        sign_in @member
+        get :show, params: { id: @customer.id }
+        expect(response).to have_http_status(200)
+      end
+
+      it 'with valid attributes' do
+        customer_params = attributes_for(:customer)
+        sign_in @member
+        expect {
+          post :create, params: { customer: customer_params }
+        }.to change(Customer, :count).by(1)
+      end
+
+      it 'Content-Type JSON' do
+        customer_params = attributes_for(:customer)
+        sign_in @member
+        post :create, format: :json, params: { customer: customer_params }
+        expect(response.content_type).to eq('application/json')
+      end
+
+      it 'Flash Notice' do
+        customer_params = attributes_for(:customer)
+        sign_in @member
+        post :create, params: { customer: customer_params }
+        expect(flash[:notice]).to match(/successfully created/)
+      end
     end
   end
 end
